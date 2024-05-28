4409f64d3b5c62817f7346f4e600c309d1a5d28a
Tomiwa <oyindml1@gmail.com>
2024-05-28 13:02:59 +0100

    commit

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -59,7 +59,9 @@ def empty_spaces():
         for row in range(3):
             for column in range(3):
                 if buttons[row][column]['text'] != "":
-                    spaces - 
+                    spaces -=1
+    if spaces == 0:
+        return False
 
 def new_game():
     pass

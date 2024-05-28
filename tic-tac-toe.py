9816fea2d8ae2b9283f9a27963cb147c4419120d
Tomiwa <oyindml1@gmail.com>
2024-05-28 13:07:37 +0100

    commit

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -36,10 +36,17 @@ def check_winner():
     for row in range(3):
         if buttons[row][0]['text'] == buttons[row][1]['text'] == buttons[row][2]['text'] != "":
             buttons[row][0].config(bg="green")
+            buttons[row][1].config(bg="green")
+            buttons[row][2].config(bg="green")
+            
             return True
         
     for column in range(3):
         if buttons[0][column]['text'] == buttons[1][column]['text'] == buttons[2][column]['text'] != "":
+            buttons[0][column].config(bg="green")
+            buttons[1][column].config(bg="green")
+            buttons[2][column].config(bg="green")
+            
             return True
         
     if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != "":
